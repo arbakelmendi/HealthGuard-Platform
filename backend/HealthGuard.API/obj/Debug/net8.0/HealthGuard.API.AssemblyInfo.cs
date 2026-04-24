d27eacbc681b6a29f887f171f70e8a8fedd3a29f
olsash <olsashala98@gmail.com>
2026-04-24 14:09:43 +0200

    feat(ui): update app favicon with HealthGuard icon

--- backend/HealthGuard.API/obj/Debug/net8.0/HealthGuard.API.AssemblyInfo.cs
+++ backend/HealthGuard.API/obj/Debug/net8.0/HealthGuard.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HealthGuard.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd9c5a9686c1ed2263689057eac646317d8130a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08923e4a6c766d37e4bd8314299b8227abda8152")]
 [assembly: System.Reflection.AssemblyProductAttribute("HealthGuard.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HealthGuard.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
